87e0e70f97f4832e4c1825f5b5f5b4a31aa0f8db
Dinara275 <dinara.akhmetzhanova@gmail.com>
2023-07-23 13:09:09 +0300

    modify gendiff, Gendiff.php, composer.json

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -7,12 +7,14 @@ $autoloadPath1 = __DIR__ . '/../../../autoload.php';
 // Путь для локальной работы с проектом
 $autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
 
+
 if (file_exists($autoloadPath1)) {
     require_once $autoloadPath1;
 } else {
     require_once $autoloadPath2;
 }
-
+function genDiff ()
+{
 $doc = <<<DOC
  
 Generate diff
@@ -27,7 +29,9 @@ Options:
 
 DOC;
 
-require('/vendor/docopt/docopt/src/docopt.php');
+require('vendor/docopt/docopt/src/docopt.php');
 $args = Docopt::handle($doc, array('version'=>'Gendiff 1.0'));
 foreach ($args as $k=>$v)
-    echo $k.': '.json_encode($v).PHP_EOL;
+	echo $k.': '.json_encode($v).PHP_EOL;
+
+}
