b323355c5ddef8c96786bf4a4c068ddd501b96f0
Dinara275 <dinara.akhmetzhanova@gmail.com>
2023-07-31 22:37:09 +0300

    Gendiff.php

--- src/Gendiff.php
+++ src/Gendiff.php
@@ -2,17 +2,8 @@
 
 namespace Gendiff\Gendiff;
 
+use Class \Gendiff\Gendiff\Docopt;
 
-$autoloadPath1 = __DIR__ . '/../../../autoload.php';
-// Путь для локальной работы с проектом
-$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
-
-
-if (file_exists($autoloadPath1)) {
-    require_once $autoloadPath1;
-} else {
-    require_once $autoloadPath2;
-}
 function genDiff ()
 {
 $doc = <<<DOC
@@ -28,8 +19,6 @@ Options:
   -v --version                  Show version
 
 DOC;
-
-use Class \Gendiff\Gendiff\Docopt; 
 
 $args = Docopt::handle($doc, array('version'=>'Gendiff 1.0'));
 foreach ($args as $k=>$v)
